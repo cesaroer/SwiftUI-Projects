a999264c6525967f44c07229247e491e2da9d7a6
cesaroer <cesarvarcard@gmail.com>
2023-05-29 23:12:31 -0600

    project init with restart

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Cesar Vargas Tapia on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
