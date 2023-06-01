0fe6d6f00ac054576b59eb735e9d52cc9235467f
cesaroer <cesarvarcard@gmail.com>
2023-06-01 14:09:07 -0600

    Added drag gesture

--- Restart/Restart/Screens/OnBoardingView.swift
+++ Restart/Restart/Screens/OnBoardingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct OnBoardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
 
     var body: some View {
         ZStack {
@@ -62,7 +64,7 @@ struct OnBoardingView: View {
                     // 3.- capsule
                     HStack {
                         Capsule().fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -77,13 +79,27 @@ struct OnBoardingView: View {
                                 .font(.system(size: 24, weight: .bold))
                         }.foregroundColor(.white)
                             .frame(width: 80, height: 80, alignment: .center)
-                            .onTapGesture {
-                                isOnboardingViewActive = false
-                            }
+                            .offset(x: buttonOffset)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged({ gesture in
+                                        if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80{
+                                            buttonOffset = gesture.translation.width
+                                        }
+                                })
+                                    .onEnded({ _ in
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        }else {
+                                            buttonOffset = 0
+                                        }
+                                    })
+                            )//: Gesture
                         Spacer()
                     }//: Hstack
                 }//: Footer
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             }//: ZStack
         }//: VStack
