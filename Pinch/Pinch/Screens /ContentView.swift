2125f4db9c0dd23390e003b8a089ddb183e91836
cesaroer <cesarvarcard@gmail.com>
2023-06-05 13:56:36 -0600

    Added scale effect by bouble tapping

--- Pinch/Pinch/Screens /ContentView.swift
+++ Pinch/Pinch/Screens /ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: -  Property
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: -  PAge Image
+                let image = #imageLiteral(resourceName: "magazine-front-cover")
+                Image(uiImage: image).resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        }else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        }.navigationViewStyle(.stack)
     }
 }
 
