87d933e4d65af8cb4be2b146b1b3c83c633e638f
cesaroer <cesarvarcard@gmail.com>
2023-06-04 23:48:50 -0600

    Added swiftUI pinch project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Cesar Vargas Tapia on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
