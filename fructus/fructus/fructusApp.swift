a68160f93037ce6d5cf2a88421f206c1b530bfea
cesaroer <cesarvarcard@gmail.com>
2023-06-07 11:02:27 -0600

    Added fruits project

---
+++ fructus/fructus/fructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fructusApp.swift
+//  fructus
+//
+//  Created by Cesar Vargas Tapia on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct fructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
