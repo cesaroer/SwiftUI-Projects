a898d0826b4350201400e114467ab3296058b975
cesaroer <cesarvarcard@gmail.com>
2023-06-12 18:01:40 -0600

    Africa app project init

---
+++ AfricaApp/AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Cesar Vargas Tapia on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
