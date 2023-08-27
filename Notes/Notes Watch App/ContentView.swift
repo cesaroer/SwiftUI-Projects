417a79530c67062a17536cff463dd15f52f15634
cesaroer <cesarvarcard@gmail.com>
2023-08-27 15:52:20 -0600

    aded apple watch project

---
+++ Notes/Notes Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Notes Watch App
+//
+//  Created by Cesar Vargas Tapia on 27/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notes/Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Cesar Vargas Tapia on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
