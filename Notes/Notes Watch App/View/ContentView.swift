18d6c893af307e96299ec75c74a67f151b39202c
cesaroer <cesarvarcard@gmail.com>
2023-08-30 23:11:06 -0600

    added load and delete method for notes

--- Notes/Notes Watch App/View/ContentView.swift
+++ Notes/Notes Watch App/View/ContentView.swift
@@ -13,8 +13,40 @@ struct ContentView: View {
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     // MARK: - FUNCTION
+    
+    func getDocumentDirectory() -> URL {
+      let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+      return path[0]
+    }
+    
     func save() {
-       dump(notes)
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed!")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory()
+                    .appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                // Do nothing
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+      withAnimation {
+        notes.remove(atOffsets: offsets)
+        save()
+      }
     }
 
     var body: some View {
@@ -46,9 +78,34 @@ struct ContentView: View {
 
             }//: HStack
             Spacer()
-            Text("\(notes.count)")
+            if notes.count >= 1 {
+                List {
+                    ForEach(notes) { note in
+                        HStack {
+                          Capsule()
+                            .frame(width: 4)
+                            .foregroundColor(.accentColor)
+                          Text(note.text)
+                            .lineLimit(1)
+                            .padding(.leading, 5)
+                        }
+                    }.onDelete(perform: delete)
+                }
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                  .resizable()
+                  .scaledToFit()
+                  .foregroundColor(.gray)
+                  .opacity(0.25)
+                  .padding(25)
+                Spacer()
+            }
         }//Vstack
         .navigationTitle("Notes")
+        .onAppear(perform: {
+          load()
+        })
     }
 }
 
