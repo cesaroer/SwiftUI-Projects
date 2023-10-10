c920110cdcb58965b194450f3cdca6e433670622
cesaroer <cesarvarcard@gmail.com>
2023-10-09 22:50:04 -0600

    Avocado project init

---
+++ Avocado/AvocadosSwiftUI/AvocadosSwiftUI/AvocadosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosSwiftUIApp.swift
+//  AvocadosSwiftUI
+//
+//  Created by Cesar Vargas Tapia on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
