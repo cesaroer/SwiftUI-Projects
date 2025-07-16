ab07c41cefc6c27de12cffa3ed16a0f7e2b66dc8
cesaroer <cesarvarcard@gmail.com>
2025-07-16 17:28:39 -0600

    Honey moon launch screen added

---
+++ Honeymoon_tinder/honeymoon/honeymoon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  honeymoon
+//
+//  Created by Cesar Vargas Tapia on 28/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Honeymoon_tinder/honeymoon/honeymoon/honeymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  honeymoonApp.swift
+//  honeymoon
+//
+//  Created by Cesar Vargas Tapia on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct honeymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
