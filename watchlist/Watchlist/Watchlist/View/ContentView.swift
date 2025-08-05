f35d451319fb007fa57f5d88f24d187bd3ce8bed
cesaroer <cesarvarcard@gmail.com>
2025-08-04 23:34:21 -0600

    Added list with data

--- watchlist/Watchlist/Watchlist/View/ContentView.swift
+++ watchlist/Watchlist/Watchlist/View/ContentView.swift
@@ -19,15 +19,54 @@ struct ContentView: View {
 
     var body: some View {
         List {
-            
+            ForEach(movies) { movie in
+                HStack {
+                    Text(movie.title)
+                    
+                    Spacer()
+                    
+                    Text(movie.genre.name)
+                }
+                .swipeActions {
+                    Button(role: .destructive) {
+                        withAnimation {
+                            modelContext.delete(movie)
+                        }
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                }
+            }
         }
         .overlay {
             if movies.isEmpty {
                 EmptyListView()
             }
         }
+        
+        // MARK: -  SAfeArea
+        .safeAreaInset(edge: .bottom, alignment: .center) {
+            Button {
+                isSheetPresented.toggle()
+            } label: {
+                ButtonImageView(symbolName: "plus.circle.fill")
+            }
+
+        }
+        
+        // MARK: -  Sheet
+        .sheet(isPresented: $isSheetPresented) {
+            NewMovieFormView()
+        }
+        
     }
 }
+
+#Preview("Sample Data") {
+  ContentView()
+    .modelContainer(Movie.preview)
+}
+
 
 #Preview {
     ContentView()
